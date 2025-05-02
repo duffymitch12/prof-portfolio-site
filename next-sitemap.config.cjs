558d0df4127c4968d458093a60919b12394a0fad
duffymitch12 <duffymitch12@gmail.com>
2025-05-02 13:07:15 -0400

    Added next-sitemap

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://mitch-duffy.vercel.app/',
+    generateRobotsTxt: true, // Also generates a robots.txt
+    changefreq: 'weekly',
+    priority: 0.7,
+  };
